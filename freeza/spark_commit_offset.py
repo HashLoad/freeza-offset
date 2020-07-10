cd388848b9b9755e0596591c9006e51ca1c6cda2
Rodrigo Bernardi <rodrigo.bernardi@ifood.com.br>
2020-07-10 09:27:43 -0300

    fix(spark_commit_offset): adjusted commit flow

--- freeza/spark_commit_offset.py
+++ freeza/spark_commit_offset.py
@@ -6,6 +6,22 @@ import time
 import threading
 
 
+def __commit_offsets(query, consumer):
+    if query.lastProgress == None:
+        return
+    for offsets in query.lastProgress['sources']:
+        end_offsets = offsets['endOffset']
+        if end_offsets == None:
+            continue
+        for topic in end_offsets.keys():
+            for partition in end_offsets[topic].keys():
+                offset = end_offsets[topic][partition]
+                topic_partition = TopicPartition(topic, int(partition))
+                consumer.assign([topic_partition])
+                consumer.commit(
+                    {topic_partition: OffsetAndMetadata(offset, None)})
+
+
 def start_commiter(query, bootstrap_servers, group_id, sleep_time=5, **kwargs):
     options = {
         "auto_offset_reset": "earliest",
@@ -16,20 +32,18 @@ def start_commiter(query, bootstrap_servers, group_id, sleep_time=5, **kwargs):
     }
 
     consumer = KafkaConsumer(**options)
-    time.sleep(5)
 
-    while query.isActive:
-        for offsets in query.lastProgress['sources']:
-            end_offsets = offsets['endOffset']
-            for topic in end_offsets.keys():
-                for partition in end_offsets[topic].keys():
-                    offset = end_offsets[topic][partition]
-                    topic_partition = TopicPartition(topic, int(partition))
-                    consumer.assign([topic_partition])
-                    consumer.commit(
-                        {topic_partition: OffsetAndMetadata(offset, None)})
+    if query == None or not query.isActive:
+        check_count = 0
+        while check_count < 5 and (query == None or not query.isActive):
+            time.sleep(5)
+            check_count += 1
+
+    while True:
+        __commit_offsets(query, consumer)
+        if not query.isActive:
+            break
         time.sleep(sleep_time)
-
 
 def start_commiter_thread(query,  bootstrap_servers, group_id, sleep_time=5, **kwargs):
     thread = threading.Thread(target=start_commiter, kwargs={
